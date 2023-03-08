d3827ddc1f0c281dcc6110bfe8b7343106eab023
LENOVO <LENOVO@Dizqy-Hidayat18>
2023-03-08 13:06:48 +0700

    Fapp

---
+++ src/main/java/com/mycompany/mib/MIB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mib;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MIB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
